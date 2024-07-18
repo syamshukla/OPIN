3ffa3bd63f00d18b8cb5ffc4e10dabef056fb149
syamshukla <53063487+syamshukla@users.noreply.github.com>
2024-07-17 20:26:04 -0400

    about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,23 +1,25 @@
 import Image from "next/image";
 export default function About() {
   return (
-    <div className="flex flex-col mt-20 mx-auto  px-4 py-8 sm:px-6 lg:px-8">
+    <div className="flex flex-col mt-20 mx-auto px-4 py-8 sm:px-6 lg:px-8">
       <h1 className="text-3xl font-bold mb-4 text-left">About Us</h1>
       <p className="mb-4 text-left">
-        Welcome to our app! We are dedicated to providing you with the best
-        service possible. Our mission is to help you find reliable information
-        and make informed decisions based on user-generated content.
+        Welcome to OPIN, your go-to search engine for opinion-based answers. We
+        are dedicated to providing you with reliable sources from various
+        conversations across the web. Our mission is to help you make informed
+        decisions based on user-generated content.
       </p>
       <h2 className="text-2xl font-semibold mb-2 text-left">Our Team</h2>
       <p className="mb-4 text-left">
-        Our team is composed of experienced professionals passionate about
-        technology and user experience. We strive to innovate and improve our
-        platform continuously.
+        OPIN is driven by a team of passionate professionals in technology and
+        user experience. We continuously innovate to enhance your search
+        experience and ensure you find the answers you seek effectively.
       </p>
       <h2 className="text-2xl font-semibold mb-2 text-left">Our Vision</h2>
       <p className="mb-4 text-left">
-        We envision a world where users can access trustworthy information
-        easily, allowing them to make better choices in their daily lives.
+        At OPIN, we envision a world where accessing trustworthy opinions is
+        effortless. We strive to empower users with the information they need to
+        make confident decisions in their daily lives.
       </p>
     </div>
   );
